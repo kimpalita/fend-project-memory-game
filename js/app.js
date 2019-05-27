0bb694d79a8cb0c26ef21c13d7bd026d91b612db
kim <kimwienands@gmail.clom>
2019-05-27 23:19:06 +0200

    added deal() function in js to deal cards

--- js/app.js
+++ js/app.js
@@ -2,13 +2,11 @@
  * Create a list that holds all of your cards
  */
 
+let cardNames = ['anchor', 'diamond', 'bomb', 'leaf', 'bolt', 'bicycle','paper-plane-o', 'cube' ];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+let cardDeck = cardNames.concat(cardNames);
+let moveCounter = 0;
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,11 +23,42 @@ function shuffle(array) {
     return array;
 }
 
+function deal(cards) {
+  moveCounter = 0;
+  shuffle(cards);
+
+  const fragment = document.createDocumentFragment();
+
+  for(card of cards) {
+    const cardContainer = document.createElement('li');
+    cardContainer.classList.add('card');
+    cardContainer.dataset.cardName = card;
+    cardContainer.innerHTML = `<i class="fa fa-${card}"></i>`;
+    fragment.appendChild(cardContainer);
+  }
+  document.querySelector('.deck').appendChild(fragment);
+}
+
+deal(cardDeck);
+
+document.querySelector('.deck').addEventListener('click', function() {
+  console.log('card has been clicked!');
+})
+
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided 'shuffle' method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+
 
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of 'open' cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
